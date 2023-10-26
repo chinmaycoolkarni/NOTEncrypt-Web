9b696be66633000463985a90f540fbaf80337e7d
chinmay.kulkarni3 <chinmay.kulkarni@thoughtworks.com>
2023-10-26 19:57:08 +0530

    Add. Initialize Backend Application

---
+++ src/test/java/com/example/NOTEncrypt/NotEncryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NOTEncrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotEncryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
