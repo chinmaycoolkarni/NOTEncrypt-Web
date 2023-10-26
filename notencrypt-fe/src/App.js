11a4cf4a64954427692cc74c557423f203f17e55
chinmay.kulkarni3 <chinmay.kulkarni@thoughtworks.com>
2023-10-26 20:02:07 +0530

    Add. Initialize Frontend Application

---
+++ notencrypt-fe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginComponent from './components/login/Login'
+
+function App() {
+  return (
+    <div className="container">
+      <LoginComponent></LoginComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ notencrypt-fe/src/components/login/Login.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import React from "react";
+
+class LoginComponent extends Component{
+
+
+    render(){
+        return(
+            <React.Fragment>
+                <div>
+                    <h1>LoginComponent</h1>
+                </div>
+            </React.Fragment>
+        )
+    }
+
+}
+
+export default LoginComponent;
