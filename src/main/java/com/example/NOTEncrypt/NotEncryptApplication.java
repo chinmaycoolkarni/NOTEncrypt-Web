b3c7e10b1f166ed12e8b81b8079285b61e757c54
chinmay.kulkarni3 <chinmay.kulkarni@thoughtworks.com>
2023-11-07 08:28:35 +0530

    Refactor. Clean Code

--- src/main/java/com/example/NOTEncrypt/NotEncryptApplication.java
+++ src/main/java/com/example/NOTEncrypt/NotEncryptApplication.java
@@ -1,7 +1,15 @@
 package com.example.NOTEncrypt;
 
+//import com.example.NOTEncrypt.entity.User;
+//import com.example.NOTEncrypt.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class NotEncryptApplication {
